72300fc4547262cb815d9fcaa164da2061d04691
Angel Antonio <antonioangel.cea.19.22@gmail.com>
2025-04-06 23:04:38 -0600

    services--

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "AIzaSyDFk5NE2wkJ1FyU3MsMJ-9nSYN-10liRTU",
+      authDomain: "novacoders-b3936.firebaseapp.com",
+      projectId: "novacoders-b3936",
+      storageBucket: "novacoders-b3936.firebasestorage.app",
+      messagingSenderId: "456963770694",
+      appId: "1:456963770694:web:e757ee611da5bd96803d47"
+    }
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyDFk5NE2wkJ1FyU3MsMJ-9nSYN-10liRTU",
+      authDomain: "novacoders-b3936.firebaseapp.com",
+      projectId: "novacoders-b3936",
+      storageBucket: "novacoders-b3936.firebasestorage.app",
+      messagingSenderId: "456963770694",
+      appId: "1:456963770694:web:e757ee611da5bd96803d47"
+    }
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  
